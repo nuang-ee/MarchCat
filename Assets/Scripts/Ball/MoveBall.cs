67dcf31501a182b10b548cc6a625bae8b8226bc7
Nuang_ee <random0411@kaist.ac.kr>
2020-01-09 21:06:25 +0900

    Initial Commit

---
+++ Assets/Scripts/Ball/MoveBall.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    public Vector2 minPos;
+    public Vector2 maxPos;
+
+    private Transform ballTf;
+    private Vector3 ballPos;
+    private Vector2 moveValue;
+    private float moveSpeed = 0.15f;
+
+    void Awake()
+    {
+        ballTf = transform;
+        ballPos = ballTf.position;
+        moveValue = Vector2.one;
+    }
+    
+    
+    void FixedUpdate() {
+        ballPos.x += moveSpeed * moveValue.x;
+        ballPos.y += moveSpeed * moveValue.y;
+        if (ballPos.x < minPos.x) {
+            moveValue.x *= -1;
+            ballPos.x += minPos.x - ballPos.x;
+        } else if (ballPos.x > maxPos.x) {
+            moveValue.x *= -1;
+            ballPos.x += maxPos.x - ballPos.x;
+        } 
+
+        if (ballPos.y < minPos.y) {
+            moveValue.y *= -1;
+            ballPos.y += minPos.y - ballPos.y;
+        } else if (ballPos.y > maxPos.y) {
+            moveValue.y *= -1;
+            ballPos.y += maxPos.y - ballPos.y;
+        } 
+        ballTf.position = ballPos;
+    }
+}
---
+++ Assets/Scripts/ChangeMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusic : MonoBehaviour
+{
+    public AudioClip level2Music;
+    private AudioSource source;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level == 2) {
+            source.clip = level2Music;
+            source.Play();
+        }
+    }
+}
