280dc9f018c4e4cdf288182606b75ddc06e678af
boingkiri <djfelrl11@kaist.ac.kr>
2020-01-11 21:17:01 +0900

    Pattern indicates beat

--- Assets/Scripts/PatternHandler.cs
+++ Assets/Scripts/PatternHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
+using AudioHelm;
 
 public class PatternHandler : MonoBehaviour
 {
@@ -14,11 +15,22 @@ public class PatternHandler : MonoBehaviour
     public int key;
     public string InstName;
 
+    private int count;
+    private Color initColor;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        //sequencer.Clear();
-
+        count = 0;
+        sequencer.Clear();
+        Color normalColor = patternElements[0].colors.normalColor;
+        initColor = new Color(normalColor.r, normalColor.g, normalColor.b);
+        sequencer.beatEvent.AddListener(delegate
+        {
+            ToggleBackgroundChange();
+        });
+        //sequencer.
         //Pattern Color Selection
         //if (InstName == "Bass Drum") {
         //    for (int )
@@ -48,7 +60,7 @@ public class PatternHandler : MonoBehaviour
             {
                 AudioHelm.Note note = sequencer.AddNote(key, selected_idx, selected_idx + 1, 1.0f);
                 existingNotes = sequencer.GetAllNotes();
-                Debug.Log("After AddNote : " + existingNotes.Count);
+                //Debug.Log("After AddNote : " + existingNotes.Count);
             }
             else {
                 
@@ -57,5 +69,25 @@ public class PatternHandler : MonoBehaviour
                 existingNotes.Remove(existing_note);
             }
         }
+    }
+
+    void ToggleBackgroundChange() {
+        ColorBlock newColorBlock;
+        if (count == 0) {
+            ColorBlock prevcolor = patternElements[15].colors;
+            prevcolor.normalColor = initColor;
+            patternElements[15].colors = prevcolor;
+        }
+        else
+        {
+            ColorBlock prevcolor = patternElements[count - 1].colors;
+            prevcolor.normalColor = initColor;
+            patternElements[count - 1].colors = prevcolor;
+        }
+        ColorBlock currentColor = patternElements[count].colors;
+        currentColor.normalColor = new Color(initColor.r + 0.2f, initColor.g + 0.2f, initColor.b + 0.2f);
+        patternElements[count].colors = currentColor;
+
+        count = (count + 1) % 16;
     }
 }
