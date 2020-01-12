368472235a4b178068c3da18f1df34d430942737
boingkiri <djfelrl11@kaist.ac.kr>
2020-01-12 16:22:03 +0900

    Keyboard tmp save
    jus tmp save

---
+++ Assets/Scripts/Key/KeySoundHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeySoundHandler : MonoBehaviour
+{
+    public Button[] White;
+    public Button[] Black;
+    public int baseKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject WhiteKeys = GameObject.Find("WhiteKey");
+        GameObject BlackKeys = GameObject.Find("BlackKey");
+        for 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
